01883112e718453d9a39414229b97fdae064fafb
Luka Milenkovic <luka.milenkovic@novelic.com>
2025-10-15 22:55:43 +0200

    Exercise 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let my_age = 33;
+    let mut fav_num: u8= 8;
+    fav_num = fav_num + 1;
+    let str = "Ovo je string";
+    let str = str.len();
+
+    println!("My age is: {}", my_age);
+    println!("Incremented fav number is: {}", fav_num);
+    println!("Len of string: {}", str);
 }
