6070feebcec122a966df89b5a4ed03354f988ec4
Luka Milenkovic <luka.milenkovic@novelic.com>
2025-10-15 23:28:19 +0200

    Exercise 2.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,34 @@ fn main() {
     let str = "Ovo je string";
     let str = str.len();
 
+    let broj8: u8 = 0xFF;
+    let broj16: u16 = 0xFFFF;
+    let broj32: u32 = 0xFFFFFF;
+    let broj128: u128 = 0xFFFFFFFFFF;
+
+    let float64 = 8.0;
+    let float32: f32 = 8.0;
+
+    let t = true;
+    let f: bool = false;
+
+    let slovo: char = 'L';
+    let smile = '😄';
+
+    println!("\nExercise 1");
     println!("My age is: {}", my_age);
     println!("Incremented fav number is: {}", fav_num);
     println!("Len of string: {}", str);
+
+    println!("\nExercise 2");
+    println!("Integer type 1: {}", broj8);
+    println!("Integer type 2: {}", broj16);
+    println!("Integer type 3: {}", broj32);
+    println!("Integer type 4: {}", broj128);
+    println!("Float ex 1: {}", float64);
+    println!("Float ex 1: {}", float32);
+    println!("Bool true: {}", t);
+    println!("Bool false: {}", f);
+    println!("Jedno slovo: {}", slovo);
+    println!("Jedan smiley: {}", smile);
 }
